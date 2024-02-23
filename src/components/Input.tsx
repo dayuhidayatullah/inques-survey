e4e451b2ee781e98de3418b2a8baa835e7177ba6
dayuhidayatullah <dayuhidayat123@gmail.com>
2024-02-23 17:42:39 +0700

    input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div className="container ">
+      <input className="border-b-2 border-b-[rgb(26, 145, 162)] w-[400px] h-[60px] focus:border-b-indigo-500 focus:outline-none "></input>
+    </div>
+  );
+};
+
+export default Input;
--- src/components/SelectionLikert.tsx
+++ src/components/SelectionLikert.tsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const SelectionLikert = () => {
-    const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
-    const temp = Array.from(alphabet.slice(0,9).values())
-    const [selectQuestion, setSelectQuestion] = useState<Number | string | null>(null)
-    const devideQuestionGrid = () => {
-        if(temp.length % 2 === 0) return 2
-        if(temp.length % 3  === 0) return 3
-        else return 2
-    }
+const SelectionLikert = ({ isRatingLikert }: { isRatingLikert: Boolean }) => {
+  const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
+  const temp = Array.from(alphabet.slice(0, 9).values());
+  const [selectQuestion, setSelectQuestion] = useState<Number | string | null>(
+    null
+  );
+  const devideQuestionGrid = () => {
+    if (temp.length % 2 === 0) return 2;
+    if (temp.length % 3 === 0) return 3;
+    else return 2;
+  };
   return (
-    <div className='container flex gap-2 mt-3'>
-        {temp.map(el => {
-            return (
-                <div className={`border-[2px] p-6 ${selectQuestion === el ? 'bg-indigo-500 text-white ': 'border-gray-500'}`} onClick={() => setSelectQuestion(el)}>
-                    {el}
-                </div>
-            )
-        })}
+    <div className={`container flex ${isRatingLikert ? "" : "gap-2"}  mt-3`}>
+      {temp.map((el, i) => {
+        return (
+          <div
+            className={`${
+              i !== 0 || i === temp.length - 1
+                ? "border-r-2 border-y-2 "
+                : "border-y-2 border-r-2 "
+            } p-6 ${selectQuestion === el ? "bg-indigo-500 text-white " : ""}
+            ${i === temp.length - 1 ? "rounded-e-md" : ""}
+            ${i === 0 ? "border-l-2 rounded-s-md" : ""}
+            `}
+            onClick={() => setSelectQuestion(el)}
+          >
+            {el}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default SelectionLikert
+export default SelectionLikert;
